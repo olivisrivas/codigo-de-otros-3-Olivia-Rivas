ea1358463b235acdf230e69d30fd966bdc6df250
Olivia Rivas~ <olivia.rivas.prf@gmail.com>
2023-12-08 10:33:58 -0600

    Corregido humildemente

--- filtro.js
+++ filtro.js
@@ -8,7 +8,7 @@ const productos = [
   {nombre: "Zapato rojo", tipo: "zapato", color: "rojo", img: "./zapato-rojo.jpg"}
 ]
 
-const li = document.getElementsByName("lista-de-productos")
+const li = document.querySelector('.lista-de-productos') //
 const $i = document.querySelector('.input');
 
 for (let i = 0; i < productos.length; i++) {
@@ -28,7 +28,7 @@ for (let i = 0; i < productos.length; i++) {
   li.appendChild(d)
 }
 
-displayProductos(productos)
+//displayProductos(productos); Se elimina esta función porque no es necesaria
 const botonDeFiltro = document.querySelector("button");
 
 botonDeFiltro.onclick = function() {
